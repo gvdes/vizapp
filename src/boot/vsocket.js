ad25cbe8e82ee9e172de67c333061685304495cf
Lalo Dev <lalo.kob@gmail.com>
2021-02-12 15:34:49 -0600

    modificacion de puerto del socket

--- src/boot/vsocket.js
+++ src/boot/vsocket.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 // const routefiles = 'http://192.168.1.86/files';
 
 const routefiles = 'http://vizappdev.grupovizcarra.net:7001/vizapidev/files';
-const vsocket_route = 'http://vsocket.grupovizcarra.net:45174';
+const vsocket_route = 'http://vsocket.grupovizcarra.net:7171';
 
 Vue.prototype.$vsocket = vsocket_route;
 Vue.prototype.$routefiles = routefiles;
