b3a2fdb35592c9bfd29a698ab0984ff250185b3b
Lalo Dev <lalo.kob@gmail.com>
2021-03-09 14:16:55 -0600

    se agregan dos reportes mas al modulo de almacenes

--- src/boot/vizapi.js
+++ src/boot/vizapi.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const vizapi = axios.create({
     // baseURL: 'http://vizapp.grupovizcarra.net:7001/vizapi/public',// build production
     baseURL: 'http://vizappdev.grupovizcarra.net:7001/vizapidev/public',// build test
-    // baseURL: 'http://192.168.1.243/public',//dev
+    // baseURL: 'http://192.168.1.243/public',//1dev
     // baseURL: 'http://192.168.1.250/vizapidev/public',//dev
     // baseURL: 'http://192.168.1.24/vizapi/public',//bebe
 });
--- src/pages/Warehouses/Reports.vue
+++ src/pages/Warehouses/Reports.vue
@@ -96,6 +96,26 @@
 					<div class="text--2 text-right q-pr-xs text-grey-7">Almacen General VS CEDIS</div>
 				</q-card>
 			</div>
+
+			<div class="col-md-3 col-xs-6 q-pb-sm q-pr-sm">
+				<q-card flat class="bg-darkl1 cursor-pointer" v-ripple @click="report(9)">
+					<q-card-section class="text-h4 text-left text-pink-14">
+						<q-spinner-puff color="orange-14" v-if="!stats"/>
+						<span v-else>{{ stats.withStock.conMaximos }}</span>
+					</q-card-section>
+					<div class="text--2 text-right q-pr-xs text-grey-7">con stock sin minimos y maximos</div>
+				</q-card>
+			</div>
+
+			<div class="col-md-3 col-xs-6 q-pb-sm q-pr-sm">
+				<q-card flat class="bg-darkl1 cursor-pointer" v-ripple @click="report(10)">
+					<q-card-section class="text-h4 text-left text-pink-14">
+						<q-spinner-puff color="orange-14" v-if="!stats"/>
+						<span v-else>{{ stats.withoutStock.negativos }}</span>
+					</q-card-section>
+					<div class="text--2 text-right q-pr-xs text-grey-7">Productos en Negativo</div>
+				</q-card>
+			</div>
 		</div>
     </q-page>
 </template>
@@ -115,7 +135,9 @@ export default {
 				'productos_sin_stock_y_con_ubicacion.xlsx',
 				'productos_con_stock_y_sin_minimos_y_maximos.xlsx',
 				'productos_con_stock_sin_exhibir.xlsx',
-				'productos_con_stock_general_vs_exhibicion.xlsx'
+				'productos_con_stock_general_vs_exhibicion.xlsx',
+				'productos_con_stock_y_con_minimo_y_maximo.xlsx',
+				'productos_con_negativos.xlsx'
 			]
         }
     },
