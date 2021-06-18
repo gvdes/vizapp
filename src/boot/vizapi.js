1a9202365abc155f38f176bdadaef6797223f407
lalokob <lalo.kob@gmail.com>
2021-06-18 13:44:01 -0500

    compilacion para app con salida por dominios

--- src/boot/vizapi.js
+++ src/boot/vizapi.js
@@ -8,9 +8,9 @@ import { io } from "socket.io-client";
 // const URLSocket = 'http://192.168.10.4:7171';
 
 // ❰❰❰❰❰ P R O D U C C I O N ❱❱❱❱❱
-const URLBackend = 'http://192.168.10.15/vizapidev/public';
-const URLFiles = 'http://192.168.10.15/vizapidev/files';
-const URLSocket = 'http://192.168.10.15:7171';
+const URLBackend = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4547/vizapidev/public';
+const URLFiles = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4547/vizapidev/files';
+const URLSocket = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:7171';
 
 // ❰❰❰❰❰ D E S A R R O L L O ❱❱❱❱❱
 // const URLBackend = 'http://192.168.1.144/vizapi/public';
