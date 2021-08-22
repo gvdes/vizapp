847853628d62cccd21026396f8469b496b21fa84
lalokob <lalo.kob@gmail.com>
2021-08-21 20:13:02 -0500

    se corrige la ruta de requests a backend

--- src/boot/vizapi.js
+++ src/boot/vizapi.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import axios from 'axios'
 
 // ❰❰❰❰❰ P R U E B A S ❱❱❱❱❱
-const URLBackend = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4544/public';
-const URLFiles = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4544/files';
+// const URLBackend = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4544/public';
+// const URLFiles = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4544/files';
 
 // const URLBackend = 'http://192.168.10.12/vizapi/public';
 // const URLFiles = 'http://192.168.10.12/vizapi/files';
+
+// ❰❰❰❰❰ P R O D U C C I O N ❱❱❱❱❱
+const URLBackend = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4547/vizapidev/public';
+const URLFiles = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4547/vizapidev/files';
 
 const vizapi = axios.create({ baseURL: URLBackend });
 
