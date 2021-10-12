4a0f3b34390f803fb2ce90d5543506108513bdd8
lalokob <lalo.kob@gmail.com>
2021-10-12 16:54:29 -0500

    Correccion en ruta de BackEnd

--- src/boot/vizapi.js
+++ src/boot/vizapi.js
@@ -6,12 +6,12 @@ import axios from 'axios'
 // const URLFiles = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4544/files';
 
 // ❰❰❰❰❰ I N   T E S T ❱❱❱❱❱
-const URLBackend = 'http://192.168.10.12/vizapi_master/public';
-const URLFiles = 'http://192.168.10.12/vizapi_master/files';
+// const URLBackend = 'http://192.168.10.12/vizapi_master/public';
+// const URLFiles = 'http://192.168.10.12/vizapi_master/files';
 
 // ❰❰❰❰❰ P R O D U C C I O N OUT❱❱❱❱❱
-// const URLBackend = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4547/vizapi_master/public';
-// const URLFiles = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4547/vizapi_master/files';
+const URLBackend = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4547/vizapi_master/public';
+const URLFiles = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4547/vizapi_master/files';
 
 const vizapi = axios.create({ baseURL: URLBackend });
 
