9cd06487a3949b1c5fc3c6416352022ffe36a9ed
lalokob <lalo.kob@gmail.com>
2022-10-12 17:11:08 -0500

    se hacen cambios locales para hacer funcionar la app en linux

--- src/boot/vizapi.js
+++ src/boot/vizapi.js
@@ -5,8 +5,8 @@ import axios from 'axios'
 // const URLBackend = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4544/public';
 // const URLFiles = 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4544/files';
 
-const URLBackend = 'http://localhost/vizapi/public';
-const URLFiles = 'http://localhost/vizapi/files';
+const URLBackend = 'http://localhost/vizapi/public/';
+const URLFiles = 'http://localhost/vizapi/files/';
 
 // ❰❰❰❰❰ P R O D U C C I O N   O U T ❱❱❱❱❱
 // const URLBackend = 'http://mx100-cedis-mkrqpwcczk.dynamic-m.com:4547/vizapi_master/public';
