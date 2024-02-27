e6c30e27f3bc9be9f18938d6d9b3d32e9d6105dc
hugomers <hugomo0897@gmail.com>
2024-02-26 19:14:06 -0600

    esta modificado el axios vizapi

--- src/boot/vizapi.js
+++ src/boot/vizapi.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-// const URLBackend = 'http://192.168.12.183/vizapi/public/';
-// const URLFiles = 'http://192.168.12.183/vizapi/files/';
+const URLBackend = 'http://192.168.10.112:1619/vizapi/public/';
+const URLFiles = 'http://192.168.10.112:1619/vizapi/files/';
 
 // const URLBackend = 'http://192.168.10.189/vizapi/public/';
 // const URLFiles = 'http://192.168.10.189/vizapi/files/';
 
 // ❰❰❰❰❰ P R O D U C C I O N   O U T ❱❱❱❱❱
-const URLBackend = 'http://mx100-cedis-mkrqpwcczk.dynamic-m.com:4646/vizapi/public';
-const URLFiles = 'http://mx100-cedis-mkrqpwcczk.dynamic-m.com:4646/vizapi/files';
+// const URLBackend = 'http://mx100-cedis-mkrqpwcczk.dynamic-m.com:4646/vizapi/public';
+// const URLFiles = 'http://mx100-cedis-mkrqpwcczk.dynamic-m.com:4646/vizapi/files';
 
 // ❰❰❰❰❰ P R O D U C C I O N  I N  P U E B L A❱❱❱❱❱
 // const URLBackend = 'http://192.168.90.253:1619/vizapi/public/';
