189cf12c50e75b4c47339dc08dee85094d298926
hugomers <hugomo0897@gmail.com>
2024-07-10 13:30:29 -0600

    se cambia el socket pa correo

--- src/boot/sockets.js
+++ src/boot/sockets.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URLprv = 'http://192.168.10.112:4141';
+const URLprv = 'http://192.168.30.253:4141';
 const URLSocket = 'http://192.168.10.189:4444';
 // const URLSocket = 'http://mx100-cedis-mkrqpwcczk.dynamic-m.com:4548';
 // const URLSocket = 'http://localhost:7171/';
