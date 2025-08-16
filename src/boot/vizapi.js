8a61ab4a838bfaa32d949580db2099334b7e8eae
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-08-16 10:07:11 -0600

    se agrega rama de ramon

--- src/boot/vizapi.js
+++ src/boot/vizapi.js
@@ -4,8 +4,12 @@ import axios from 'axios'
 // const URLBackend = 'http://192.168.10.112:1619/vizapi/public/';
 // const URLFiles = 'http://192.168.10.112:1619/vizapi/files/';
 
-const URLBackend = 'http://192.168.10.189/vizapi/public/';
-const URLFiles = 'http://192.168.10.189/vizapi/files/';
+// const URLBackend = 'http://192.168.10.189/vizapi/public/';
+// const URLFiles = 'http://192.168.10.189/vizapi/files/';
+
+const URLBackend = 'http://192.168.40.214:1619/vizapi/public/';
+const URLFiles = 'http://192.168.40.214:1619/vizapi/files/';
+
 
 // const URLBackend = 'http://192.168.10.160:1619/vizapi/public/';
 // const URLFiles = 'http://192.168.10.160:1619/vizapi/files/';
